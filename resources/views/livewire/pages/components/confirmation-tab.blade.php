0643b9d93fafffdf4aa4d5c5c901adb344f7728b
Jhonieeeel <villacurajhonieeeel@gmail.com>
2025-08-03 20:26:06 +0800

    UI: Border fix

--- resources/views/livewire/pages/components/confirmation-tab.blade.php
+++ resources/views/livewire/pages/components/confirmation-tab.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-white sm:mt-12 sm:px-6 border ">
+<div class="bg-white sm:mt-12 sm:px-6">
     <div class="sm:py-6 flex items-center gap-x-1.5">
         <p class="font-medium text-md">Confirmation Tab</p>
         <x-icon name="check" class="w-5 h-5" outline />
@@ -9,7 +9,7 @@
                 <button type="button" wire:click="$set('activeTab', 'tab1')"
                     class="-mb-px py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium text-center border rounded-t-lg disabled:opacity-50 disabled:pointer-events-none
         {{ $activeTab === 'tab1'
-            ? 'bg-white border-b-transparent text-gray-600 border-gray-200 hover:text-gray-600'
+            ? 'bg-white border-b-transparent text-gray-600 border-gray-200  hover:text-gray-600'
             : 'bg-gray-50 text-gray-500 border-gray-200 hover:text-gray-700' }}"
                     id="tab1" aria-selected="{{ $activeTab === 'tab1' ? 'true' : 'false' }}">
                     User Request
@@ -27,7 +27,7 @@
         </div>
     @endif
 
-    <div class="mt-3">
+    <div class="border  border-t-transparent {{ $activeTab ? 'border-b' : 'border-b-0' }}">
         <div id="card-type-tab-preview" role="tabpanel" aria-labelledby="card-type-tab-item-1">
             @if ($activeTab === 'tab1')
                 @if ($requisitions)
@@ -37,8 +37,8 @@
 
                     @foreach ($groupedRequisitions as $requisitionId => $items)
                         <div>
-                            <div class="w-full flex items-start gap-x-6 py-6">
-                                <div class="w-full border rounded p-4">
+                            <div class="w-full  flex items-start gap-x-6 sm:px-2 sm:py-4">
+                                <div class="w-full rounded p-4 border">
                                     <div class="flex items-center justify-between shadow">
                                         <h3 class="text-sm font-medium">Requested Items</h3>
                                         <h3 class="text-sm font-medium">Requested Quantity</h3>
@@ -59,7 +59,7 @@
                                     </div>
                                 </div>
 
-                                <div class="w-full border rounded p-4">
+                                <div class="w-full rounded p-4 border">
                                     <div class="flex items-center justify-between shadow">
                                         <h3 class="text-sm font-medium">Requested By</h3>
                                         <h3 class="text-sm font-medium">Approved By</h3>
@@ -100,7 +100,7 @@
                         </div>
                     @endforeach
                 @else
-                    <div class="w-full flex justify-center items-start gap-x-6 py-6">
+                    <div class="w-full  flex justify-center items-start gap-x-6">
                         <p class="text-sm text-center text-gray-500 font-medium">No selected Request</p>
                     </div>
                 @endif
@@ -111,5 +111,6 @@
             @endif
         </div>
     </div>
+    <div class="pt-5"></div>
 </div>
 
