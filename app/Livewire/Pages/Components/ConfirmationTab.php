3d1bd4e470cf972d9d101e2d78b7a9fcc4c65e72
Jhonieeeel <villacurajhonieeeel@gmail.com>
2025-08-05 16:51:38 +0800

    Requisition need to be refactored

--- app/Livewire/Pages/Components/ConfirmationTab.php
+++ app/Livewire/Pages/Components/ConfirmationTab.php
@@ -7,26 +7,28 @@ use App\Models\RequisitionItem;
 use Livewire\Attributes\On;
 use Livewire\Attributes\Session;
 use Livewire\Component;
+
 class ConfirmationTab extends Component
-{   
+{
     public $requisitions = [];
 
     public $selectedRequisition;
+    public $requisitionStatus = false;
 
-    public $activeTab;   
+    public $activeTab;
 
-    public function changeTab($tab) 
+    public function changeTab($tab)
     {
         $this->activeTab = $tab;
         $this->dispatch('selectedRequisition', requisition: $this->selectedRequisition, tab: $tab);
     }
 
     #[On('selectedRequisition')]
-    public function setRequisition($requisition, $tab) 
+    public function setRequisition($requisition, $tab)
     {
-        $this->selectedRequisition = $requisition;  
+        $this->selectedRequisition = $requisition;
         $this->activeTab = $tab;
-        
+
         $this->requisitions = RequisitionItem::with('stock.supply', 'requisition')
             ->whereHas('requisition', function ($query) {
                 $query->where('requested_by', $this->selectedRequisition);
--- resources/views/livewire/pages/components/confirmation-tab.blade.php
+++ resources/views/livewire/pages/components/confirmation-tab.blade.php
@@ -5,25 +5,26 @@
             <h3 class="text-lg font-semibold text-gray-800">Requisition Confirmation</h3>
             <x-icon name="check-circle" class="w-5 h-5 text-green-500" solid />
         </div>
-        <div class="border-b border-gray-200">
+        <div class="border-b border-gray-300">
             <nav class="flex gap-x-1" aria-label="Tabs" role="tablist" aria-orientation="horizontal">
                 <button type="button" wire:click="changeTab('tab1')"
-                    class="-mb-px py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium text-center border rounded-t-lg disabled:opacity-50 disabled:pointer-events-none
+                    class="-mb-px py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium text-center border border-gray-300 rounded-t-lg disabled:opacity-50 disabled:pointer-events-none
         {{ $activeTab === 'tab1'
-            ? 'bg-white border-b-transparent text-gray-600 font-semibold border-gray-200  hover:text-gray-600'
-            : 'bg-gray-50 text-gray-500  border-gray-200 hover:text-gray-700' }}"
+            ? 'bg-white border-b-transparent text-gray-600 border-gray-300  hover:text-gray-600'
+            : 'bg-gray-50 text-gray-500  border-gray-300 hover:text-gray-700' }}"
                     id="tab1" aria-selected="{{ $activeTab === 'tab1' ? 'true' : 'false' }}">
-                    Request Details
+                    Requisition
                 </button>
-
-                <button type="button" wire:click="changeTab('tab2')"
-                    class="-mb-px py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium text-center border rounded-t-lg disabled:opacity-50 disabled:pointer-events-none
+                @if ($requisitions->first()->requisition->status)
+                    <button type="button" wire:click="changeTab('tab2')"
+                        class="-mb-px py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium text-center border rounded-t-lg disabled:opacity-50 disabled:pointer-events-none
         {{ $activeTab === 'tab2'
-            ? 'bg-white border-b-transparent text-gray-600 font-semibold border-gray-200 hover:text-gray-600'
-            : 'bg-gray-50 text-gray-500 border-gray-200 hover:text-gray-700' }}"
-                    id="tab2" aria-selected="{{ $activeTab === 'tab2' ? 'true' : 'false' }}">
-                    Requisition PDF
-                </button>
+            ? 'bg-white border-b-transparent text-gray-600 border-gray-300 hover:text-gray-600'
+            : 'bg-gray-50 text-gray-500 border-gray-300 hover:text-gray-700' }}"
+                        id="tab2" aria-selected="{{ $activeTab === 'tab2' ? 'true' : 'false' }}">
+                        Requisition PDF
+                    </button>
+                @endif
             </nav>
         </div>
     @else
@@ -33,7 +34,7 @@
         </div>
     @endif
 
-    <div class="border  border-t-transparent {{ $activeTab ? 'border-b' : 'border-b-0' }}">
+    <div class="border border-gray-300  border-t-transparent {{ $activeTab ? 'border-b' : 'border-b-0' }}">
         <div id="card-type-tab-preview" role="tabpanel" aria-labelledby="card-type-tab-item-1">
             @if ($activeTab === 'tab1')
                 @if ($requisitions)
@@ -42,72 +43,14 @@
                     @endphp
 
                     @foreach ($groupedRequisitions as $requisitionId => $items)
-                        <div>
-                            <div class="w-full  flex items-start gap-x-6 sm:px-2 sm:py-4">
-                                <div class="w-full rounded p-4 border">
-                                    <div class="flex items-center justify-between shadow">
-                                        <h3 class="text-sm font-medium">Requested Items</h3>
-                                        <h3 class="text-sm font-medium">Requested Quantity</h3>
-                                    </div>
-                                    <hr>
-                                    <div class="pt-2.5">
-                                        <ul class="list-disc list-inside text-gray-800 space-y-1.5">
-                                            @foreach ($items as $item)
-                                                <li class="list-none flex items-center justify-between">
-                                                    <span class="text-sm">
-                                                        {{ $item->stock->supply->name }}
-                                                    </span>
-                                                    <x-badge flat amber label="{{ $item->quantity }}" />
-                                                </li>
-                                            @endforeach
-                                        </ul>
-                                    </div>
-                                </div>
+                        <div class="w-full sm:px-2 sm:py-4 rounded">
+                            <div class="flex flex-col">
+                                <div>
 
-                                <div class="w-full rounded p-4 border">
-                                    <div class="flex items-center justify-between shadow">
-                                        <h3 class="text-sm font-medium">Requested By</h3>
-                                        <h3 class="text-sm font-medium">Approved By</h3>
-                                        <h3 class="text-sm font-medium">Issued By</h3>
-                                        <h3 class="text-sm font-medium">Received By</h3>
-                                    </div>
-                                    <hr>
-                                    <div class="pt-2.5">
-                                        <ul
-                                            class="list-disc list-inside text-gray-800 space-y-1.5 flex items-center justify-between">
-                                            <li class="list-none">
-                                                <span class="text-sm">
-                                                    {{ $items->first()->requisition->requestedBy->name }}
-                                                </span>
-                                            </li>
-                                            <li class="list-none">
-                                                <span
-                                                    class="text-sm {{ $items->first()->requisition->approved_by ? 'text-gray-700' : 'text-gray-400' }}">
-                                                    {{ optional($items->first()->requisition->approvedBy)->name ?? 'No user' }}
-                                                </span>
-                                            </li>
-                                            <li class="list-none">
-                                                <span
-                                                    class="text-sm {{ $items->first()->requisition->issued_by ? 'text-gray-700' : 'text-gray-400' }}">
-                                                    {{ optional($items->first()->requisition->issuedBy)->name ?? 'No user' }}
-                                                </span>
-                                            </li>
-                                            <li class="list-none">
-                                                <span
-                                                    class="text-sm {{ $items->first()->requisition->received_by ? 'text-gray-700' : 'text-gray-400' }}">
-                                                    {{ optional($items->first()->requisition->receivedBy)->name ?? 'No user' }}
-                                                </span>
-                                            </li>
-                                        </ul>
-                                    </div>
                                 </div>
                             </div>
                         </div>
                     @endforeach
-                @else
-                    <div class="w-full  flex justify-center items-start gap-x-6">
-                        <p class="text-sm text-center text-gray-500 font-medium">No selected Request</p>
-                    </div>
                 @endif
             @elseif($activeTab === 'tab2')
                 <div class="w-full flex justify-center items-start gap-x-6 py-6">
